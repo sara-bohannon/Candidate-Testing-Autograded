216daa69252d7e0ad225bf5fb3802127b63cbcd4
Your Name <you@example.com>
2024-05-09 18:43:56 -0500

    classThree

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,33 +1,43 @@
-const input = require('readline-sync');
+const input = require('readline-sync')
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = " "
+
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? "
+let correctAnswer = "Sally Ride"
+let candidateAnswer = " "
+
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = "Who was the first American woman in space? "
+let correctAnswers = "Sally Ride"
+let candidateAnswers = " "
 
 
-function askForName() {
+
+
   // TODO 1.1b: Ask for candidate's name //
+  function askForName(){
+    candidatename = input.question("Enter you name: ");
+  }
+
+function askQuestion(question) {
+  console.log(candidateAnswer);
 
 }
-
-function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
-}
+
+
 
 function gradeQuiz(candidateAnswers) {
+  
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
@@ -42,10 +52,12 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log(`Hello, ${firstName} ${lastName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+ 
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {

50ef630e30ce1927e98be547041846c0feaef808
Your Name <you@example.com>
2024-05-23 16:53:32 -0500

    added functions

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,40 +2,54 @@ const input = require('readline-sync')
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName = (" ")
 
+// TODO 1.1a: Define candidateName // 
+let candidateName = " "
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? "
-let correctAnswer = "Sally Ride"
-let candidateAnswer =(" ")
+let question = [
+  "1) Who was the first American woman in space? ",
+  "2) True or False: 5000 meters = 5 kilometers.",
+  "3) (5 + 3)/2 * 10 = ?" , 
+  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "5) What is the minimum crew size for the International Space Station (ISS)? "
+];
 
 
 
 //TODO: Variables for Part 2
-let questions = "Who was the first American woman in space? "
-let correctAnswers = "Sally Ride"
-let candidateAnswers = (" ")
+
 
 
 
 
   // TODO 1.1b: Ask for candidate's name //
   function askForName(){
-    nameQuery = input.question("Enter your name: ");
+    candidateName = input.question("Enter your name: ");
   }
 
 function askQuestion() {
-  questionQuery = input.question(question)
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question(questions[i]));
+    console.log('Your answer: ${candidateAnswers[i]}\n');
+  }
 }
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
 
 function gradeQuiz(candidateAnswers) {
-  
+  let numCorrect = 0;
+
+  for (let i = 0; i < questions.length; i++) {
+    console.log('Question ${i + 1}; ${questions[i]}');
+    console.log('Correct Answers: ${correctAnswers[i]}');
+    console.log('Your Answer: ${candidateAnswers[i]}\n');
+
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    }
+  }
+ 
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
